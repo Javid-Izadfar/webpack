bd681c0965f475024aa3e95a3a10df73e9bd18c6
Javid Izadfar <Javid.Izadfar@gmail.com>
2017-07-03 22:46:49 +0430

    add some PostCSS plugins

--- template/build/vue-loader.conf.js
+++ template/build/vue-loader.conf.js
@@ -10,14 +10,16 @@ module.exports = {
     extract: isProduction
   }),
   postcss: [
-    require('postcss-import'),
-    require('postcss-each'),
-    require('postcss-mixins'),
-    require('postcss-nested'),
-    require('postcss-at-rules-variables'),
-    require('postcss-for'),
-    require('postcss-custom-properties'),
-    require('postcss-simple-vars'),
-    require('postcss-short')
+    require('postcss-import')(),
+    require('postcss-each')(),
+    require('postcss-mixins')(),
+    require('postcss-nested')(),
+    require('postcss-at-rules-variables')(),
+    require('postcss-for')(),
+    require('postcss-custom-properties')(),
+    require('postcss-simple-vars')(),
+    require('postcss-short')(),
+    require('postcss-calc')(),
+    require('postcss-color-function')()
   ]
 }
